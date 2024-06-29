b26702843a767bfbac2397b38e0771704bdde4e6
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-06-29 15:07:25 +0200

    Create Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h1>Welcome to My Project</h1>
+            <p>Your one-stop solution for all your needs.</p>
+        </div>
+    );
+}
+
+export default Home;
